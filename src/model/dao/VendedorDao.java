35ed8a0411fdae3892174f1a789f207077a66f7d
Luiz Felipe <luizfelipepintorr@gmail.com>
2022-06-27 21:23:02 -0300

    Criando as interfaces VendedorDao e DepartamentoDao

---
+++ src/model/dao/VendedorDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import java.util.List;
+
+import model.entidades.Vendedor;
+
+public interface VendedorDao {
+	void insert(Vendedor obj);
+	void update(Vendedor obj);
+	void deleteId(Integer id);
+	Vendedor procureId(Integer id);
+	List<Vendedor> procureTodos();
+}
